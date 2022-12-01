96a56a9398299cbd3010480f124428e1cc078b9c
Alex Wong <alex-wzm@outlook.com>
2022-12-01 14:35:51 -0600

    Add output to gen script

--- gen/proto/generate.sh
+++ gen/proto/generate.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-GO_OUT_RELATIVE_PATH="../../service-go/internal/api"
+SERVICE_RELATIVE_PATH="../../service-go/internal/api"
+SERVICE_CLIENT_PATH="../../client-go/internal/api"
 
-protoc --go_out=$GO_OUT_RELATIVE_PATH --go_opt=paths=source_relative \
-    --go-grpc_out=$GO_OUT_RELATIVE_PATH --go-grpc_opt=paths=source_relative \
+protoc --go_out=$SERVICE_RELATIVE_PATH --go_opt=paths=source_relative \
+    --go-grpc_out=$SERVICE_RELATIVE_PATH --go-grpc_opt=paths=source_relative \
+    interview/interview.proto
+
+protoc --go_out=$SERVICE_CLIENT_PATH --go_opt=paths=source_relative \
+    --go-grpc_out=$SERVICE_CLIENT_PATH --go-grpc_opt=paths=source_relative \
     interview/interview.proto
